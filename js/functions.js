21aea38f2a7a0a466ffacd002e34fb3716d7455a
rellenos <danieling00@gmail.com>
2024-06-14 10:04:11 +0200

    initial commit

---
+++ js/functions.js
@@ -0,0 +1,14 @@
+let tauler = [
+    ["document.getElementById('grid00')","document.getElementById('grid01')","document.getElementById('grid02')"],
+    ["document.getElementById('grid10')","document.getElementById('grid11')","document.getElementById('grid12')"],
+    ["document.getElementById('grid20')","document.getElementById('grid21')","document.getElementById('grid22')"]
+];
+
+tauler[0][0] = "X";
+console.log(tauler);
+
+for (i=0;i<3;i++) {
+    for (j=0;j<3;j++) {
+        // console.log(tauler[[i],[j]])
+    } 
+}
