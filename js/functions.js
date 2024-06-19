aa0695bd76506b625eaed8bd574a8eec9f81b535
rellenos <danieling00@gmail.com>
2024-06-19 09:17:24 +0200

    funcion validar

--- js/functions.js
+++ js/functions.js
@@ -4,6 +4,7 @@ let tauler = [
     ["","",""],
     ["","",""]
 ]
+let ganador = false;
 
 // for (i=0;i<3;i++) {
 //     for (j=0;j<3;j++) {
@@ -25,7 +26,6 @@ for (var i = 1; i <= 9; i++) {
         var columna = coordenada.charAt(6);
         // console.log(coordenada);
         // console.log("el primer valor de coordenada es " + columna + " y el segundo es " + fila)
-
         if (turno%2 == 0) {
             this.classList.add("turnoA")
             tauler[fila][columna] = "X";
@@ -34,14 +34,36 @@ for (var i = 1; i <= 9; i++) {
             this.classList.add("turnoB")
             tauler[fila][columna] = "O";
         }
-        ganar();
         turno++;
         // console.log(turno);
-        console.log(tauler)
+        console.log(tauler);
+        validar();
+        if (ganador) alert("has guanyat");
     }
 }
 
-function ganar() {
+function validar() {
+    
+    // console.log(ganador);
+    for (let i = 0; i < 3; i++) {
+        if((tauler[i][0]==tauler[i][1])&&(tauler[i][0]==tauler[i][2])&&(tauler[i][0]!="")) {
+            ganador = true;
+            console.log(ganador);
+        }
+        if((tauler[0][i]==tauler[1][i])&&(tauler[0][i]==tauler[2][i])&&(tauler[0][i]!="")) {
+            ganador = true;
+            console.log(ganador);
+        }
+        if((((tauler[0][0]==tauler[1][1])&&(tauler[0][0]==tauler[2][2]))
+        ||  ((tauler[0][2]==tauler[1][1])&&(tauler[0][2]==tauler[2][0])))
+        &&  (tauler[1][1]!="")) {
+            ganador = true;
+            console.log(ganador);
+        }
+    }
+}
+
+/*function ganar() {
     var mensajeGanar = document.getElementById("mensajeGanar");
     var colorGanador = document.getElementById("colorGanador");
 
@@ -49,7 +71,6 @@ function ganar() {
         case "X":
             mensajeGanar.style.display = "block";
             colorGanador.innerHTML = "azul";
-            
             break;
     }
     switch (tauler[0][0] && tauler[0][1] && tauler[0][2]) {
@@ -58,4 +79,4 @@ function ganar() {
             document.getElementById("colorGanador").innerHTML = "naranja";
             break;
     }
-}
+}*/
